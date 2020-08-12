6730da269dd1fc7fc6fec63e2439c2a134664aed
Tirth Patel <tirthpatel7498@gmail.com>
2020-08-12 19:59:05 -0400

    Create code.py
    Added simple code which checks if the number given by the user is an armstrong number.

---
+++ code/code.py
@@ -0,0 +1,17 @@
+#take user input
+number = int(input("Enter the number you want to check for armstrong number: "))
+
+numOfDigits = len(str(number))
+
+sum = 0
+copy = number
+
+while copy > 0:
+    temp = copy % 10
+    sum += temp ** numOfDigits
+    copy //= 10
+
+if sum == number:
+    print(True)
+else:
+    print(False)
