fe824aee01e3a19040e194c9b2870a935346fbdd
Tirth Patel <36417825+tirthpatel7498@users.noreply.github.com>
2020-08-13 08:46:09 -0400

    Tirth changes (#2)
    * Create code.py
    
    Added simple code which checks if the number given by the user is an armstrong number.
    
    * Update code.py
    
    Added comments to the code.
    
    * Update code.py
    
    Added armstrong function to the code.
    For ease in __init__.py

---
+++ code/code.py
@@ -0,0 +1,26 @@
+# The program checks whether a number is an Armstrong number or not
+# eg: if 407 = 4^3 + 0^3 + 7^3 then the number is an armstrong number.
+
+
+def armstrong(number):
+    numOfDigits = len(str(number))
+    sum = 0
+    copy = number
+
+    while copy > 0:
+        temp = copy % 10
+        sum += temp ** numOfDigits
+        copy //= 10
+
+    if sum == number:
+        return True
+    else:
+        return False
+
+
+def main():
+    number = int(input("Enter the number you want to check for armstrong number: "))
+    print(armstrong(number))
+
+if __name__ == "__main__":
+    main()
