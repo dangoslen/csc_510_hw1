c6378dc792978b4713fcfd27f50d0fe8ff148dab
harshkachhadia <harsh.lions@gmail.com>
2020-08-13 02:33:02 -0400

    created setup.py & init.py, added data, updated requirements.txt and readme files.

---
+++ setup.py
@@ -0,0 +1,27 @@
+
+from setuptools import setup
+
+setup(
+    name='csc_510_hw1',
+    version='0.1',
+    description='Learning how to build a cool github repo', 
+    author='Harsh Kachhadia',
+    author_email='hmkachha@ncsu.edu',
+    url='https://github.com/dangoslen/csc_510_hw1',
+
+	  packages=find_packages(include=['code', 'code.*','data']),
+    long_description="""Learning how to build a cool github repo""",
+    classifiers=[
+          "License :: MIT License",
+          "Programming Language :: Python",
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Topic :: cool github repo",
+      ],
+    keywords='csc-510 hw1 csc-510-hw1',
+    license='MIT License',	
+    install_requires=['numpy'], #numpy is just for sample
+    tests_require=['pytest'],
+    package_data={'data':['sample_data.json']}
+    )
+
