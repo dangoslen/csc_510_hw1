c56d8fc479db85ffa0ec3c0fa6ea73f9ad26d18c
Tirth Patel <tirthpatel7498@gmail.com>
2020-08-12 20:02:54 -0400

    Update code.py
    Added comments to the code.

--- code/code.py
+++ code/code.py
@@ -1,6 +1,10 @@
+# The program checks whether a number is an Armstrong number or not
+# eg: if 407 = 4^3 + 0^3 + 7^3 then the number is an armstrong number.
+
 #take user input
 number = int(input("Enter the number you want to check for armstrong number: "))
 
+# find the order of the input number 
 numOfDigits = len(str(number))
 
 sum = 0
